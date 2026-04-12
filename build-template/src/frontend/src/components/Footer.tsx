6877d475648468a7c5658a9cd6293dfee1989bac
varunnn9tirumala <varuntirumala570@gmail.com>
2026-04-12 20:04:29 +0530

    Clean repo and remove node_modules

--- build-template/src/frontend/src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-black text-white text-center py-6 mt-20">
-      <p>Trip Craft © 2026 • AI Travel Planner designed for SRMAP students for reserach • All Copyrights were taken</p>
-    </footer>
-  )
-}
